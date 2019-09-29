5130248eeacdd3ca0af30c69ccc502860b5f053b
mikelgarciaurbina <mikelgarciaurbina@gmail.com>
2019-09-29 17:30:44 +0200

    Update expo sdk to 35

--- src/navigation/MainNavigator.js
+++ src/navigation/MainNavigator.js
@@ -1,4 +1,4 @@
-import { createStackNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
 
 import CoinsScreen from '../screens/Coins';
 import HomeScreen from '../screens/Home';
